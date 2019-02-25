340087baf311cf006c24387e28407028b9e6e6f5
Свирин Никита Александрович <svirin@root.ians.aero>
2019-02-25 18:21:13 +0300

    Added configuration layout

--- src/ViewLayer/Configuration/ConfigurationView.cpp
+++ src/ViewLayer/Configuration/ConfigurationView.cpp
@@ -1,13 +1,22 @@
 #include "ConfigurationView.h"
 #include "ConfigurationViewPrivate.h"
 
+#include <QCheckBox>
+#include <QGroupBox>
+#include <QTextEdit>
+#include <QVBoxLayout>
+
 using ViewLayer::ConfigurationViewPrivate;
 using ViewLayer::ConfigurationView;
 
 
-ConfigurationViewPrivate::ConfigurationViewPrivate(QWidget* parent)
+ConfigurationViewPrivate::ConfigurationViewPrivate(QWidget* parent) :
+    protocolFilterGroupBox(new QGroupBox(parent)),
+    tcpCheckBox(new QCheckBox("TCP", parent)),
+    udpCheckBox(new QCheckBox("UDP", parent)),
+    ipCheckBox(new QCheckBox("IP filter", parent)),
+    ipTextEdit(new QTextEdit)
 {
-    Q_UNUSED(parent);
 }
 
 
@@ -18,6 +27,38 @@ ConfigurationView::ConfigurationView(QWidget *parent) :
     m_pimpl(new ConfigurationViewPrivate(parent))
 {
     setObjectName("configurationView");
+
+    m_pimpl->protocolFilterGroupBox->setTitle("Protocol");
+
+    m_pimpl->tcpCheckBox->setChecked(true);
+    m_pimpl->udpCheckBox->setChecked(true);
+    m_pimpl->ipCheckBox->setChecked(false);
+    m_pimpl->ipTextEdit->setReadOnly(true);
+
+    QVBoxLayout* groupBoxLayout = new QVBoxLayout;
+    groupBoxLayout->addWidget(m_pimpl->tcpCheckBox);
+    groupBoxLayout->addWidget(m_pimpl->udpCheckBox);
+    groupBoxLayout->addStretch(0);
+    m_pimpl->protocolFilterGroupBox->setLayout(groupBoxLayout);
+
+    QVBoxLayout* protocolLayout = new QVBoxLayout;
+    protocolLayout->addWidget(m_pimpl->protocolFilterGroupBox);
+    protocolLayout->addStretch(1);
+
+    QVBoxLayout* ipLayout = new QVBoxLayout;
+    ipLayout->addWidget(m_pimpl->ipCheckBox);
+    ipLayout->addWidget(m_pimpl->ipTextEdit);
+    ipLayout->addStretch(1);
+
+    QHBoxLayout* layout = new QHBoxLayout;
+    layout->addLayout(protocolLayout);
+    layout->addLayout(ipLayout);
+    layout->addStretch(1);
+    setLayout(layout);
+
+    connect(m_pimpl->ipCheckBox, &QCheckBox::toggled, [this] (bool checked) {
+        m_pimpl->ipTextEdit->setReadOnly(!checked);
+    });
 }
 
 ConfigurationView::~ConfigurationView()
--- src/ViewLayer/Configuration/ConfigurationViewPrivate.h
+++ src/ViewLayer/Configuration/ConfigurationViewPrivate.h
@@ -1,7 +1,11 @@
 #ifndef CONFIGURATION_VIEW_PRIVATE_H
 #define CONFIGURATION_VIEW_PRIVATE_H
 
+class QCheckBox;
+class QGroupBox;
+class QTextEdit;
 class QWidget;
+
 
 namespace ViewLayer
 {
@@ -12,6 +16,31 @@ struct ConfigurationViewPrivate
 {
     ConfigurationViewPrivate(QWidget* parent);
     ConfigurationViewPrivate(const ConfigurationViewPrivate&) = delete;
+
+    /**
+     * @brief рамка для фильтрации по протоколу
+     */
+    QGroupBox* protocolFilterGroupBox = nullptr;
+
+    /**
+     * @brief фильтрация для tcp
+     */
+    QCheckBox* tcpCheckBox = nullptr;
+
+    /**
+     * @brief фильтрация для udp
+     */
+    QCheckBox* udpCheckBox = nullptr;
+
+    /**
+     * @brief фильтрация для ip
+     */
+    QCheckBox* ipCheckBox = nullptr;
+
+    /**
+     * @brief список валидных ip в случае фильтрации
+     */
+    QTextEdit* ipTextEdit = nullptr;
 };
 }
 
